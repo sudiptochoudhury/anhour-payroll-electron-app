d635134c6efc4a825ab68b462279c7bb5fea4f64
SudiptoChoudhury <mail@sudipto.net>
2018-07-24 06:46:17 +0530

    [update] settings and show disconnected server notice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {app, BrowserWindow, Menu} from 'electron';
+import {app, BrowserWindow, dialog, Menu} from 'electron';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -34,20 +34,41 @@ const createWindow = () => {
     backgroundColor: '#fff'
   });
 
-    // and load the index.html of the app.
+  // and load the index.html of the app.
   mainWindow.loadURL(`http://payroll.anhoursolution.local:8888`);
+
+  const contents = mainWindow.webContents;
+  contents.on('did-fail-load', (event, errorCode, errorDescription) => {
+    if (splash && splash.destroy) {
+      splash.destroy();
+    }
+    const choice = dialog.showMessageBox(mainWindow, {
+      type: 'error',
+      buttons: ['Retry', 'Exit'],
+      title: 'Ops, something nasty has happened!',
+      message: 'Unable to connect to server! Check if your server is accessible!',
+      defaultId: 0,
+      cancelId: 1
+    }, (index) => {
+      if (index) {
+        mainWindow.close();
+      } else {
+        mainWindow.reload();
+      }
+    });
+  });
 
   // if main window is ready to show, then destroy the splash window and show up the main window
   mainWindow.once('ready-to-show', () => {
     setTimeout(() => {
       splash.destroy();
       splash = null;
-      }, 2000);
+    }, 2000);
     mainWindow.show();
   });
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
